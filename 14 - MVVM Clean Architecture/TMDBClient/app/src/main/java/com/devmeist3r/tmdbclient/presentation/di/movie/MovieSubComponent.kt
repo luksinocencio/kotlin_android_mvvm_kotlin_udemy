345fe7711656c556475719e445cd3a52b30418bc
devmeist3r <devmeist3r@gmail.com>
2021-02-06 06:20:01 -0300

    *

--- 14 - MVVM Clean Architecture/TMDBClient/app/src/main/java/com/devmeist3r/tmdbclient/presentation/di/movie/MovieSubComponent.kt
+++ 14 - MVVM Clean Architecture/TMDBClient/app/src/main/java/com/devmeist3r/tmdbclient/presentation/di/movie/MovieSubComponent.kt
@@ -3,8 +3,6 @@ package com.devmeist3r.tmdbclient.presentation.di.movie
 import com.devmeist3r.tmdbclient.presentation.movie.MovieActivity
 import dagger.Subcomponent
 
-
-
 @MovieScope
 @Subcomponent(modules = [MovieModule::class])
 interface MovieSubComponent {
--- 14 - MVVM Clean Architecture/TMDBClient/app/src/main/java/com/devmeist3r/tmdbclient/presentation/movie/MovieActivity.kt
+++ 14 - MVVM Clean Architecture/TMDBClient/app/src/main/java/com/devmeist3r/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,9 +3,13 @@ package com.devmeist3r.tmdbclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.devmeist3r.tmdbclient.R
 import com.devmeist3r.tmdbclient.databinding.ActivityMovieBinding
 import com.devmeist3r.tmdbclient.presentation.di.Injector
@@ -14,9 +18,11 @@ import javax.inject.Inject
 class MovieActivity : AppCompatActivity() {
 
   @Inject
-  private lateinit var factory: MovieViewModelFactory
+  lateinit var factory: MovieViewModelFactory
+
   private lateinit var movieViewModel: MovieViewModel
   private lateinit var binding: ActivityMovieBinding
+  private lateinit var adapter: MovieAdapter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -28,11 +34,31 @@ class MovieActivity : AppCompatActivity() {
     movieViewModel = ViewModelProvider(this, factory)
       .get(MovieViewModel::class.java)
 
-    val responseLiveData = movieViewModel.getMovies()
-    responseLiveData.observe(this, Observer {
-      Log.i("MYTAG", it.toString())
-    })
+    initRecyclerView()
   }
 
+  private fun initRecyclerView() {
+    binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+    adapter = MovieAdapter()
+    binding.movieRecyclerView.adapter = adapter
+    displayPopularMovies()
+  }
+
+  private fun displayPopularMovies() {
+    binding.movieProgressBar.visibility = View.VISIBLE
+    val responseLiveData = movieViewModel.getMovies()
+
+    responseLiveData.observe(this, Observer {
+//      Log.i("MYTAG", it.toString())
+      if (it != null) {
+        adapter.setList(it)
+        adapter.notifyDataSetChanged()
+        binding.movieProgressBar.visibility = View.GONE
+      } else {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        Toast.makeText(this, "No avaliable data", Toast.LENGTH_LONG).show()
+      }
+    })
+  }
 
 }
