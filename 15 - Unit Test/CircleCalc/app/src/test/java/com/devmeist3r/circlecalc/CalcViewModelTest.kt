0f3addab936701e664cc8d2d1e265586620e06a7
devmeist3r <devmeist3r@gmail.com>
2021-02-07 03:15:21 -0300

    *

---
+++ 15 - Unit Test/CircleCalc/app/src/test/java/com/devmeist3r/circlecalc/CalcViewModelTest.kt
@@ -0,0 +1,43 @@
+package com.devmeist3r.circlecalc
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+class CalcViewModelTest {
+
+    private lateinit var calcViewModel: CalcViewModel
+    private lateinit var calculations: Calculations
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        calculations = Mockito.mock(Calculations::class.java)
+        Mockito.`when`(calculations.calculateArea(2.1))
+                .thenReturn(13.8474)
+        Mockito.`when`(calculations.calculateCircumference(1.0))
+                .thenReturn(6.28)
+        calcViewModel = CalcViewModel(calculations)
+    }
+
+    @Test
+    fun calculateArea_radiusSent_updateLiveData() {
+        calcViewModel.calculateArea(2.1)
+        var result = calcViewModel.areaValue.value
+        assertThat(result).isEqualTo("13.8474")
+    }
+
+    @Test
+    fun calculateCircumference_radiusSent_updateLiveData() {
+        calcViewModel.calculateCircumference(1.0)
+        var result = calcViewModel.circumferenceValue.value
+        assertThat(result).isEqualTo("6.28")
+    }
+
+
+}
